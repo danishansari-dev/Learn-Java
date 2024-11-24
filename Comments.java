6fd12ba4ed3979bb09b8b762d39d5501fcac1124
Danish Ansari <137506936+danish9491@users.noreply.github.com>
2024-11-24 15:44:19 +0530

    adding more files

---
+++ Comments.java
@@ -0,0 +1,12 @@
+public class Comments {
+    public static void main(String[] args) {
+        // Single line comment
+        // This is my comment
+        // I won't execute
+
+        /*  Multil
+            line
+            comment
+        */
+    }
+}
---
+++ Sum_of_ab.java
@@ -0,0 +1,31 @@
+import java.util.*;
+
+public class Sum_of_ab {
+    public static void main(String[] args) {
+        // int a = 10;
+        // int b = 5;
+        // int sum = a+b;
+        // System.out.println(sum);
+        
+        Scanner sc = new Scanner(System.in);
+        // int a = sc.nextInt();
+        // int b = sc.nextInt();
+        // int sum = a+b;
+        // System.out.println(sum);
+
+        System.out.print("Enter number 1: ");
+        int a = sc.nextInt();
+        System.out.print("Enter number 2: ");
+        int b = sc.nextInt();
+        int sum = a+b;
+        System.out.println("Sum of a and b is: "+sum);
+        
+        
+        // System.out.print("Enter number 1 and 2: ");
+        // int a = sc.nextInt();
+        // int b = sc.nextInt();
+        // int sum = a+b;
+        // System.out.println("Sum of a and b is: "+sum);
+
+    }   
+}
