6e378cb452cb39401561c7b5fce915ed822844e7
Danish Ansari <137506936+danish9491@users.noreply.github.com>
2024-11-25 07:21:33 +0530

    add operators

---
+++ Operators/operators.java
@@ -0,0 +1,37 @@
+package Operators;
+
+public class operators {
+    public static void main(String[] args) {
+    /*
+    Arithmetic Operators (binary/Unary)
+        Binary - 2 operands [+,-,*,/,%]
+
+        Unary - ++, -- 
+            --a, ++a -> pre-incriment
+            a--, a++ -> post incriment
+
+    Relational Operators
+        == equal to
+        != not equal to (Inequality)
+        > Greater than..
+        < Less than..
+        >= Greater than or equal to
+        <= Less than or equal to
+
+    Logical Operators
+        && -> Logical AND [f f -> f]
+        || -> Logical OR
+        ! -> Logical NOT
+        
+    Bitwise Operators
+    
+    Assignment Operators
+        = 
+        += -> a=a+b
+        -=
+        *=
+        /=
+    */
+    }
+    
+}
