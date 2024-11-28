07f16f950014882af8593e63a66d359cf0b15d9a
Danish Ansari <137506936+danish9491@users.noreply.github.com>
2024-11-28 22:39:08 +0530

    Adding Half Pyramid.java

---
+++ Loops/Pattern_Questions/Q3_Half_Pyramid.java
@@ -0,0 +1,16 @@
+
+public class Q3_Half_Pyramid {
+    public static void main(String[] args) {
+        // Scanner sc = new Scanner(System.in);
+        // System.out.print("Enter number: ");
+        // int n = sc.nextInt();
+        int n = 3;
+        for(int i=1; i<=n; i++) {
+            for(int j=i; j<=n; j++) {
+                System.out.print(i);
+            }
+        System.out.println("");
+        }
+    }
+    
+}
