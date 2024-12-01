defea756ea6e5f5d49372df35229afaead1e7e62
Danish Ansari <137506936+danish9491@users.noreply.github.com>
2024-12-01 08:22:33 +0530

    add function file codes

---
+++ Functions_and_Methods/Syntax_code.java
@@ -0,0 +1,8 @@
+public class Syntax_code {
+    public static void printHelloWorld() {
+        System.out.println("Hello World");
+    }
+    public static void main(String[] args) {
+        printHelloWorld();
+    }
+}
