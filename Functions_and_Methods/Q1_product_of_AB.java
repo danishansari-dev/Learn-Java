7e45e6c251eae3d6146f61a3a871e53bd8d91a50
Danish Ansari <137506936+danish9491@users.noreply.github.com>
2024-12-02 07:13:30 +0530

    add product of two numbers.java

---
+++ Functions_and_Methods/Q1_product_of_AB.java
@@ -0,0 +1,11 @@
+public class Q1_product_of_AB {
+    public static int multiply(int a, int b) {
+        int product = a*b;
+        return product;
+    }
+    public static void main(String[] args) {
+        int pro =  multiply(5, 5);
+        System.out.println(pro);
+    }
+    
+}
