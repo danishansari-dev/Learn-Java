3f47863f471229ad36e3de6488899649292078b0
Danish Ansari <137506936+danish9491@users.noreply.github.com>
2024-12-05 07:44:08 +0530

    Code update

--- Conditional_statement/Q7_binary_to_decimal.java
+++ Conditional_statement/Q7_binary_to_decimal.java
@@ -1,5 +1,20 @@
-public static void main(String[] args) {
+public class Q7_binary_to_decimal {
+
+    public static void binToDec(int binNum) {
+        int pow = 0;
+        int decNum = 0;
+        int myNum = binNum;
+
+        while(binNum>0) {
+            int lastDigit = binNum%10;
+            decNum = decNum + (int)(lastDigit * Math.pow(2, pow));
+
+            pow++;
+            binNum = binNum/10;   //binNum /=10;
+        }
+        System.out.println("Decimal of "+ myNum+" = "+decNum);
+    }
     public static void main(String[] args) {
-        
+        binToDec(101);
     }
 }
--- notes.md
+++ notes.md
@@ -1,4 +1,4 @@
-Lat digit -> num % 10
+Last digit -> num % 10
 remove last digit -> num/10
 
 (n%10==0) -> Divisible by 10
