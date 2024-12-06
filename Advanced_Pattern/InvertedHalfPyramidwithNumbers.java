97d4bee15bd70624bc265a9df64e49e50650f6ce
Danish Ansari <137506936+danish9491@users.noreply.github.com>
2024-12-06 07:35:33 +0530

    Create InvertedHalfPyramidwithNumbers.java

---
+++ Advanced_Pattern/InvertedHalfPyramidwithNumbers.java
@@ -0,0 +1,9 @@
+public class InvertedHalfPyramidwithNumbers {
+    public static void InvertedHalfPyramidwithNumbers(int n) {
+        
+    }
+    public static void main(String[] args) {
+        
+    }
+    
+}
