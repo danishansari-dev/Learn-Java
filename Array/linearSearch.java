1382183610a1ebcd68753b5a7a6e539e480104ad
Danish Ansari <137506936+danish9491@users.noreply.github.com>
2024-12-07 16:20:29 +0530

    Update linearSearch.java

--- Array/linearSearch.java
+++ Array/linearSearch.java
@@ -3,7 +3,7 @@ public class linearSearch {
         
         for(int i=0; i<numbers.length; i++) {
             if(numbers[i] == key) {
-                return 1;
+                return i;
             }
         }
 
@@ -12,7 +12,7 @@ public class linearSearch {
 
     public static void main(String args[]) {
         int numbers[] = {2, 4, 6, 8, 10, 12, 14, 16};
-        int key = 12;
+        int key = 10;
     
         int index = linearSearch(numbers, key);
         if(index == -1) {
