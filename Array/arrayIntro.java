ef8ff4586626a1152867e31029027cf8e52e24ac
Danish Ansari <137506936+danish9491@users.noreply.github.com>
2024-12-07 16:09:07 +0530

    Create arrayIntro.java

---
+++ Array/arrayIntro.java
@@ -0,0 +1,12 @@
+public class arrayIntro {
+
+    public static void main(String[] args) {
+
+        int marks[] = new int[50];
+        int moreNumbers[] = {4, 5, 6};
+        String fruits[] = {"aple", "mango", "orange"};
+        System.out.println(fruits[1]);
+
+    }
+    
+}
