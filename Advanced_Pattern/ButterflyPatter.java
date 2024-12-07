079b22d565c7a61d8350221fd6899b5e3d2f2105
Danish Ansari <137506936+danish9491@users.noreply.github.com>
2024-12-07 06:30:32 +0530

    Update ButterflyPatter.java

--- Advanced_Pattern/ButterflyPatter.java
+++ Advanced_Pattern/ButterflyPatter.java
@@ -1,9 +1,11 @@
 public class ButterflyPatter {
     public static void butterflu_pattern(int n) {
-        
+        for(int i=0; i<=n; i++) {
+            
+        }
     }
     public static void main(String[] args) {
-        1
+    
     }
     
 }
