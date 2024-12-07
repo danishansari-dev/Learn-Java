146514dd524914c7f1460bd23a9ee251a61e61d6
Danish Ansari <137506936+danish9491@users.noreply.github.com>
2024-12-07 07:02:52 +0530

    Update ButterflyPatter.java

--- Advanced_Pattern/ButterflyPatter.java
+++ Advanced_Pattern/ButterflyPatter.java
@@ -1,11 +1,39 @@
 public class ButterflyPatter {
     public static void butterflu_pattern(int n) {
-        for(int i=0; i<=n; i++) {
-            
+        for(int i=1; i<=n; i++) {
+            // starts - i
+            for(int j=1; j<=i; j++){
+                System.out.print("*");
+            }
+            // spaces - 2*(n-i)
+            for(int j=1; j<=2*(n-i); j++){
+                System.out.print(" ");
+            }
+            // starts - i
+            for(int j=1; j<=i; j++){
+                System.out.print("*");
+            }
+            System.out.println();
+        }
+
+        for(int i=n; i>=1; i--) {
+            // starts - i
+            for(int j=1; j<=i; j++){
+                System.out.print("*");
+            }
+            // spaces - 2*(n-i)
+            for(int j=1; j<=2*(n-i); j++){
+                System.out.print(" ");
+            }
+            // starts - i
+            for(int j=1; j<=i; j++){
+                System.out.print("*");
+            }
+            System.out.println();
         }
     }
     public static void main(String[] args) {
-    
+    butterflu_pattern(4);
     }
     
 }
