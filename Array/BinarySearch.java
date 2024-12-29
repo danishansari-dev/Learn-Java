68084d58cb969fd332ea7bd5e8cd45095d997a0d
Mohammad Danish Ansari <danishaftab.help@gmail.com>
2024-12-29 14:22:37 +0530

    Update BinarySearch.java

--- Array/BinarySearch.java
+++ Array/BinarySearch.java
@@ -3,7 +3,7 @@ public class BinarySearch {
     public static int BinarySearch(int n) {
         return -1;
     } 
-
     public static void main(String[] args) {
+        BinarySearch(4);
     }
 }
