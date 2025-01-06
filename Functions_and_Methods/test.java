abe55334abe44b3e34a2f577e55a58ea76c4aa7c
Mohammad Danish Ansari <danishaftab.help@gmail.com>
2025-01-06 14:35:13 +0530

    Create test.java

---
+++ Functions_and_Methods/test.java
@@ -0,0 +1,9 @@
+publie class test.java {
+    public static int products(int a, int b) {
+        int product = a*b;
+        return product;
+    }
+    public static void main(String[] args) {
+        System.out.println(products92,3);
+    }
+}
