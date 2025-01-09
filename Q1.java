ef22a69e4d1fb499147d8a0e7cf0ee1853d33040
Mohammad Danish Ansari <danishaftab.help@gmail.com>
2025-01-09 23:08:51 +0530

    Create Q1.java

---
+++ Q1.java
@@ -0,0 +1,10 @@
+public class Q1 {
+    public static void main(String[] args) {
+        // Print numbers from 1 to 10
+        int num = 1;
+        while(num<11) {
+            System.out.println(num);
+            num++;
+        }
+    }
+}
