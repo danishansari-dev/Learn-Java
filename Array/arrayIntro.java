3b85a496a00e7a8881d01b474b3ebd493d0c5801
Mohammad Danish Ansari <danishaftab.help@gmail.com>
2025-01-13 15:31:32 +0530

    Update arrayIntro.java

--- Array/arrayIntro.java
+++ Array/arrayIntro.java
@@ -4,7 +4,9 @@ public class arrayIntro {
 
         int marks[] = new int[50];
         int moreNumbers[] = {4, 5, 6};
-        String fruits[] = {"aple", "mango", "orange"};
+        String fruits[] = {"apple", "mango", "orange"};
+        System.out.println(marks[0]);
+        System.out.println(moreNumbers[1]);
         System.out.println(fruits[1]);
 
 
