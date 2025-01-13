0cb12b5112e914514d4f4367a3fe6545aaa90cca
Mohammad Danish Ansari <danishaftab.help@gmail.com>
2025-01-13 23:26:23 +0530

    Create basic_programs.java

---
+++ Array/basic_programs.java
@@ -0,0 +1,20 @@
+
+public class basic_programs {
+    public static int countOccurance(int arr[], int x) {
+        int count =0;
+        for(int i=0; i<arr.length; i++) {
+            if(arr[i] == x) {
+                count++;
+            }
+        }
+        return count;
+    }
+    public static void main(String[] args) {
+        int arr[] = {1, 2, 5, 3, 5, 6, 5};
+        int x = 5;
+        System.out.println(countOccurance(arr, x));
+
+
+    }
+    
+}
