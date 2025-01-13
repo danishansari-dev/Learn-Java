13161f774d2c4368cf0e269aa2a358983f438681
Mohammad Danish Ansari <danishaftab.help@gmail.com>
2025-01-13 22:23:40 +0530

    Update largestNumber.java

--- Array/largestNumber.java
+++ Array/largestNumber.java
@@ -1,18 +1,18 @@
 public class largestNumber {
     public static int getLargest(int numbers[]) {
-        int largeest = Integer.MIN_VALUE; // - infinity
+        int largest = Integer.MIN_VALUE; // - infinity
         int smalest = Integer.MAX_VALUE;
 
         for(int i=0; i<numbers.length; i++) {
-            if(numbers[i]>largeest) {
-                largeest = numbers[i];
+            if(numbers[i]>largest) {
+                largest = numbers[i];
             }
             if(numbers[i]<smalest) {
                 smalest = numbers[i];
             }
         }
         System.out.println("Smalest number is : " + smalest);
-        return largeest;
+        return largest;
     }
 
     public static void main(String[] args) {
