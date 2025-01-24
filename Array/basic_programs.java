b1f9cbdc2d2efc768d096421cc09b58aa7e0176d
Mohammad Danish Ansari <danishaftab.help@gmail.com>
2025-01-24 12:24:42 +0530

    Update basic_programs.java

--- Array/basic_programs.java
+++ Array/basic_programs.java
@@ -1,3 +1,4 @@
+import java.util.Arrays;
 
 public class basic_programs {
     public static int countOccurance(int arr[], int x) {
@@ -26,19 +27,46 @@ public class basic_programs {
         return lastIndex;
     }
 
-    // Count the number of elements strictly greater than value x.
-    public static int countGreaterValues(int arr[], int x) {
+    // chech wheather arr is shorted or not
+    public static boolean isShorted(int arr[]) {
+        boolean check = true;
+        for(int i=1; i<arr.length; i++) {
+            if(arr[i] < arr[i-1]) {
+                check = false;
+                break;
+            }
+        }
+        return check;
+    }
 
-        
+    // Sort function 
+    public static void sort(int arr[]) {
+        Arrays.sort(arr);
+        for(int i=0; i<arr.length; i++) {
+            System.out.print(arr[i]+ " ");
+        }
+    }
+    // Count the number of elements strictly greater than value x.
+    public static int strictlyGreater(int arr[], int x) {
+        int count = 0;
+        for(int i=0; i<arr.length; i++) {
+            if(arr[i] > x) {
+                count++;
+            }
+        }
+        return count;
     }
 
     public static void main(String[] args) {
-        int arr[] = {1, 2, 3, 4, 5};
+        int arr[] = {1, 2, 3, 4, 5, 5};
         int x = 3;
 
         System.out.println("Count of "+x+" is : " + countOccurance(arr, x));
         System.out.println("Last occurance of "+ x+ " is "+ lastOccurance(arr, x));
-        System.out.println(countGreaterValues(arr, x));
+        System.out.println("Strictly Greater than then value " +x+ " is " +lastOccurance(arr, x));
+        System.out.println("is shorted? " + isShorted(arr));
+        System.out.println(sort(arr));
+
     }
-    
+
 }
