408f735b5d08f829033fe73fae902b7710bd8fe4
Mohammad Danish Ansari <danishaftab.help@gmail.com>
2025-01-24 12:57:59 +0530

    Update Syntax_with_parameters.java

--- Functions_and_Methods/Syntax_with_parameters.java
+++ Functions_and_Methods/Syntax_with_parameters.java
@@ -8,10 +8,10 @@ public class Syntax_with_parameters {
     }
     
     public static void main(String[] args) {
-        Scanner sc = new Scanner(System.in);
+        Scanner s = new Scanner(System.in);
         System.out.print("Enter two numbers: ");
-        int a = sc.nextInt();
-        int b = sc.nextInt();
+        int a = s.nextInt();
+        int b = s.nextInt();
         int sum = calculateSum(a, b); // Arguments or actula parameters
         System.out.println("Sum is " + sum);
     }
