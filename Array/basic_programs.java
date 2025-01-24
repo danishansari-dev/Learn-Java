57bbc40552c67433a0e237be8828d1deefcc798c
Mohammad Danish Ansari <danishaftab.help@gmail.com>
2025-01-24 16:59:01 +0530

    Update basic_programs.java

--- Array/basic_programs.java
+++ Array/basic_programs.java
@@ -1,5 +1,4 @@
-import java.util.Arrays;
-import java.util.Scanner;
+import java.util.*;
 
 public class basic_programs {
     public static int countOccurance(int arr[], int x) {
@@ -28,6 +27,17 @@ public class basic_programs {
         return lastIndex;
     }
 
+    // Count the number of elements strictly greater than value x.
+    public static int strictlyGreater(int arr[], int x) {
+        int count = 0;
+        for(int i=0; i<arr.length; i++) {
+            if(arr[i] > x) {
+                count++;
+            }
+        }
+        return count;
+    }
+
     // chech wheather arr is shorted or not
     public static boolean isShorted(int arr[]) {
         boolean check = true;
@@ -47,27 +57,26 @@ public class basic_programs {
             System.out.print(arr[i]+ " ");
         }
     }
-    // Count the number of elements strictly greater than value x.
-    public static int strictlyGreater(int arr[], int x) {
-        int count = 0;
-        for(int i=0; i<arr.length; i++) {
-            if(arr[i] > x) {
-                count++;
-            }
-        }
-        return count;
-    }
-
+    
     public static void main(String[] args) {
-        Scanner sc = new Scanner(System.in);
         int arr[] = {1, 2, 3, 4, 5, 5};
         int x = 3;
+        
+        Scanner sc = new Scanner(System.in);
+        System.out.print("Enter size of array: ");
+        int n = sc.nextInt();
 
+        int arr1[] = new int[n];
+        System.out.println("Enter " +n+ " elements: ");
+        for(int i=0; i<arr1.length; i++) {
+            arr1[i] = sc.nextInt();
+        }
+        
         System.out.println("Count of "+x+" is : " + countOccurance(arr, x));
         System.out.println("Last occurance of "+ x+ " is "+ lastOccurance(arr, x));
         System.out.println("Strictly Greater than then value " +x+ " is " +lastOccurance(arr, x));
         System.out.println("is shorted? " + isShorted(arr));
-        System.out.println(sort(arr));
+        sort(arr1);
 
     }
 
