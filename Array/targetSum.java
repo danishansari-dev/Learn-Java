b4d6a0006e66526faa117f08f4ef10a3e56af562
Mohammad Danish Ansari <danishaftab.help@gmail.com>
2025-01-25 19:44:06 +0530

    Update targetSum.java

--- Array/targetSum.java
+++ Array/targetSum.java
@@ -14,6 +14,20 @@ public class targetSum {
         return ans;
     }
 
+    public static int tripletSum(int arr[], int target) {
+        int ans= 0;
+        for(int i=0; i<arr.length; i++) { // first number
+            for(int j=i+1; j<arr.length; j++) {  //Second number
+                for(int k=j+1; k<arr.length; k++) { // third number
+                    if (arr[i] + arr[j] + arr[k] == target) {
+                        ans++;
+                    }
+                }
+            }
+        }
+        return ans;
+    }
+
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
         System.out.print("Enter size of array: ");
@@ -27,7 +41,8 @@ public class targetSum {
 
         System.out.print("Enter target number: ");
         int target = sc.nextInt();
-        System.out.println(targetSum(arr, target));
+        // System.out.println(targetSum(arr, target));
+        System.out.println("Ans is = "+tripletSum(arr, target));
 
     }
 }
