15f3b4c4ca3b870a8bb32b084cd76fb87e2376ce
Mohammad Danish Ansari <danishaftab.help@gmail.com>
2025-01-27 23:11:02 +0530

    Update basicProgramsL17.java

--- Array/basicProgramsL17.java
+++ Array/basicProgramsL17.java
@@ -27,6 +27,24 @@ public class basicProgramsL17 {
         System.out.println("b: " +b);
     }
 
+    public static int[] rotateArray(int arr[], int d) {
+        int n = arr.length;
+        int temp[] = new int[d];
+        for(int i=0; i<d; i++) {
+            temp[i] = arr[i];
+        }
+
+        for(int i=d; i<n; i++) {
+            arr[i-d] = arr[i];
+        }
+
+        for(int i=0; i<d; i++) {
+            arr[n-d+i] = temp[i];
+        }
+
+        return arr;
+    }
+
     public static void main(String[] args) {
         // Scanner sc = new Scanner(System.in);
         // System.out.print("Enter size of an array: ");
@@ -43,8 +61,6 @@ public class basicProgramsL17 {
         // swap(a, b);
         swapWithoutTemp(a,b);
 
-        int arr[] = {1, 2, 3, 4, 5,6};
-        
-
+        // int arr[] = {1, 2, 3, 4, 5,6};
     }
 }
