876315d77866c746f12451ea1cecd5ddc3ba274c
Mohammad Danish Ansari <danishaftab.help@gmail.com>
2025-01-29 23:55:11 +0530

    Update twoPointersL18.java

--- Array/twoPointersL18.java
+++ Array/twoPointersL18.java
@@ -4,6 +4,7 @@ public class twoPointersL18 {
         for(int i=0; i<arr.length; i++) {
             System.out.print(arr[i]+ " ");
         }
+        System.out.println();
     }
     public static void main(String[] args) {
         
