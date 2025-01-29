0535f464dad177e66b52004de8699a1f3b637d87
Mohammad Danish Ansari <danishaftab.help@gmail.com>
2025-01-29 23:53:39 +0530

    Update twoPointersL18.java

--- Array/twoPointersL18.java
+++ Array/twoPointersL18.java
@@ -1,6 +1,10 @@
 public class twoPointersL18 {
 
-    public static
+    public static void printArray(int arr[]) {
+        for(int i=0; i<arr.length; i++) {
+            
+        }
+    }
     public static void main(String[] args) {
         
     }
