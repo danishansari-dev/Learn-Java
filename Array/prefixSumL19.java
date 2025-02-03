a11593045bf4391ab777512be5576d654d0e4777
Mohammad Danish Ansari <danishaftab.help@gmail.com>
2025-02-04 01:17:48 +0530

    Create prefixSumL19.java

---
+++ Array/prefixSumL19.java
@@ -0,0 +1,36 @@
+import java.util.Scanner;
+public class prefixSumL19 {
+
+    public static void printArray(int arr[]) {
+        for(int i=0; i<arr.length; i++) {
+            System.out.print(arr[i] + " ");
+        }
+        System.out.println();
+    }
+
+    public static int[] prefixSum(int arr[]) {
+        int n = arr.length;
+        int pref[] = new int[n];
+        pref[0] = arr[0];
+        for(int i=1; i<n; i++) {
+            pref[i] = pref[i-1] + arr[i];
+        }
+        return pref;
+    }
+    public static void main(String[] args) {
+
+        Scanner sc = new Scanner(System.in);
+        System.out.print("Enter size of array: ");
+        int n = sc.nextInt();
+
+        int arr[] = new int[n];
+        System.out.print("Enter "+n+ " elements: ");
+        for(int i=0; i<n; i++) {
+            arr[i] = sc.nextInt();
+        }
+
+        int ans[] = prefixSum(arr);
+        printArray(ans);
+    }
+}
+
