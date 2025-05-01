94e924a2c7f9dfa83d29d412d00c823d25e83a43
Mohammad Danish Ansari <danishaftab.help@gmail.com>
2025-05-01 19:22:07 +0530

    Update Q1.java

--- Loops/Q1.java
+++ Loops/Q1.java
@@ -3,7 +3,7 @@ public class Q1 {
     public static void main(String[] args) {
         // Print numbers from 1 to 10
         int num = 1;
-        while(num<11) {
+        while(num<101) {
             System.out.println(num);
             num++;
         }
