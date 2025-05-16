14779e21487ad5c2a4151849a1372411cc6556df
Mohammad Danish Ansari <danishaftab.help@gmail.com>
2025-05-16 18:53:10 +0530

    Update pascalsTriangle.java

--- 2D_Arrays/pascalsTriangle.java
+++ 2D_Arrays/pascalsTriangle.java
@@ -2,36 +2,36 @@ import java.util.Scanner;
 
 public class pascalsTriangle {
 
-    public static void printMatrix(int[][] matrix){
-        for(int i=0; i<matrix.length; i++){
-            for(int j=0; j<matrix[i].length; j++){
+    public static void printMatrix(int[][] matrix) {
+        for (int i = 0; i < matrix.length; i++) {
+            for (int j = 0; j < matrix[i].length; j++) {
                 System.out.print(matrix[i][j] + " ");
             }
             System.out.println();
         }
     }
 
-    public static int[][] pascall(int n){
+    public static int[][] pascall(int n) {
         int ans[][] = new int[n][];
 
-        for(int i = 0; i<n; i++){
-            ans[i] = new int[i+1];
+        for (int i = 0; i < n; i++) {
+            ans[i] = new int[i + 1];
             ans[i][0] = ans[i][i] = 1;
 
-            for(int j=1; j<i; j++){
-                ans[i][j] = ans[i-1][j] + ans[i-1][j-1];
+            for (int j = 1; j < i; j++) {
+                ans[i][j] = ans[i - 1][j] + ans[i - 1][j - 1];
             }
         }
         return ans;
     }
+
     public static void main(String[] args) {
 
         Scanner sc = new Scanner(System.in);
         System.out.println("Enter n: ");
-        
         int n = sc.nextInt();
         sc.close();
-        int  ans[][] = pascall(n);
+        int ans[][] = pascall(n);
         printMatrix(ans);
     }
 }
