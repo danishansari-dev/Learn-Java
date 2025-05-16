6dc924707b900d49dbfc395e73cc1467647c6441
Mohammad Danish Ansari <danishaftab.help@gmail.com>
2025-05-16 17:20:27 +0530

    Update rought_code.java

--- rought_code.java
+++ rought_code.java
@@ -1,55 +1,102 @@
-
 public class rought_code {
-
-    public static int linearSearch(int arr[], int key) {
-        for(int i=1; i<arr.length; i++) {
-            if(arr[i] == key) {
-                return i;
-            }
-        }
-        return -1;
-    }
-
-    public static int largestNum(int arr[], int key) {
-        int largest = Integer.MIN_VALUE;
-        int smallest = Integer.MAX_VALUE;
-
-        for(int i=0; i<arr.length; i++) {
-            if(arr[i]>largest) {
-                largest = arr[i];
-            }
-            if(arr[i]<smallest) {
-                smallest = arr[i];
-            }
-        }
-        System.out.println("Smallest value is "+ smallest);
-        return largest;
-    }
-
-    public static int maxNum(int arr[]) {
-        int ans = 0;
-        for(int i=0; i<arr.length; i++) {
-            if(arr[i]>ans){
-                ans = arr[i];
-            }
-        }
-        return ans;
-    }
-
     public static void main(String[] args) {
-        int arr[] = { 2,3,4,5,6,6,8,3,9,10,19,12};
-        int key = 9;
 
-        System.out.println("largest value is: " + largestNum(arr, key));
 
-        int index = linearSearch(arr, key);
-        if(index == -1) {
-            System.out.println("Key not found");
-        } else {
-            System.out.println("Key is at index: " + index);
-        }
+            // System.out.println("   J    a   v     v  a");
+            // System.out.println("   J   a a   v   v  a a");
+            // System.out.println("J  J  aaaaa   V V  aaaaa");
+            // System.out.println(" JJ  a     a   V  a     a");
 
-        System.out.println("Max num is: " + maxNum(arr));
+            // System.out.println(((25.5 * 3.5 - 3.5 * 3.5) / (40.5 - 4.5)));
+            // System.out.println(4.0 * (1 - (1.0/3) + (1.0/5) - (1.0/7) + (1.0/9) - (1.0/11)));
 
+            // double r = 7.5;
+            // double pi = Math.PI;
+            // System.out.println("Perimeter is = " + 2*pi*r);
+            // System.out.println("Area is = "+ pi*r*r);
+            
+            // float a,b,c;
+            // a = 3;
+            // b = 5;
+            // c = 6;
+            // System.out.println((a+b+c)/3f);
+
+        //     System.out.println("* * * * * * ==================================");
+        //     System.out.println(" * * * * *  ==================================");
+        //     System.out.println("* * * * * * ==================================");
+        //     System.out.println(" * * * * *  ==================================");
+        //     System.out.println("* * * * * * ==================================");
+        //     System.out.println(" * * * * *  ==================================");
+        //     System.out.println("* * * * * * ==================================");
+        //     System.out.println("==============================================");
+        //     System.out.println("==============================================");
+        //     System.out.println("==============================================");
+        //     System.out.println("==============================================");
+        //     System.out.println("==============================================");
+        //     System.out.println("==============================================");
+
+        // int a = 5;
+        // int b = 7;
+        // int temp = a;
+        // a = b;
+        // b = temp;
+        // System.out.println("a = " + a + "\nb = " +b);
+
+        // a = a^b;
+        // b = a^b;
+        // a = a^b;
+        // System.out.println("after swap\na = " + a + "\nb = " +b);
+
+        // System.out.println(" +\"\"\"\"\"+");
+        // System.out.println("[| o o |]");
+        // System.out.println(" |  ^  | ");
+        // System.out.println(" | '-' |");
+        // System.out.println(" +-----+");
+
+        // int a = 25;
+        // int b = 39;
+        // System.out.println("25 != 29");
+        // System.out.println("25 <= 39");
+        // System.out.println("25 < 39");
+
+        // int num = 25;
+        // int a = num/10; // remove last digit
+        // int b = num%10; //last digit
+        // System.out.println(a+b);
+
+        // int sum = 0;
+        // while(num != 0) {
+        //     int a = num%10;
+        //     sum = sum+a;
+        //     num = num/10;
+        // }
+        // System.out.println(sum);
+
+        // System.out.println("Twinkle, twinkle, little star,\n \t How I wonder what you are!\n\t\t Up above the world so high,\n \t\t Like a diamond in the sky.\nTwinkle, twinkle, little star, \n\t How I wonder what you are");
+
+        // int n = 100;
+        // int output = n + n+(n*10) + n+(n*10)+(n*100);
+        // System.out.println(n + " + " + (n + (n * 10)) + " + " + (n + (n * 10) + (n * 100)));
+        
+        // for(int i = 1; i<=n; i++) {
+        //     if(i%2 != 0) {
+        //         System.out.print(i+", ");
+        //     }
+        // }
+
+        //     if(n%2 == 0) {
+            //         System.out.println("1");
+            //     } else {
+                //         System.out.println("0");
+                //     }
+                
+        // int n = 100;
+        // for(int i=1; i<=n; i++){
+        //     if(i%3 == 0 && i%5 == 0){
+        //         System.out.print(i + ", ");
+        //     }
+        // }
+
+        
     }
 }
