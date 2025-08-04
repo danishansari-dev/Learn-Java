58ed8c9ea378ebfe3fc595f04fc6e45a42b862e9
Mohammad Danish Ansari <danishaftab.help@gmail.com>
2025-08-04 18:06:07 +0530

    Add Q5_spiralOrder.java

--- 2D_Arrays/Q5_spiralOrder.java
+++ 2D_Arrays/Q5_spiralOrder.java
@@ -1,7 +1,7 @@
 import java.util.Scanner;
 
 public class Q5_spiralOrder {
-
+    
     public static void printMatrix(int[][] matrix){
         for(int i = 0; i < matrix.length; i++) {
             for(int j = 0; j < matrix[i].length; j++){
@@ -10,45 +10,64 @@ public class Q5_spiralOrder {
         System.out.println("");
         }
     }
-
+    
     public static void printSpiralOrder(int matrix[][], int r, int c) {
-        int topRow = 0, bottomRow = r-1, leftCol = 0, rightCol = r-1;
+        int topRow = 0, bottomRow = r-1, leftCol = 0, rightCol = c-1;
         int totalElements = 0;
 
         while (totalElements < r * c) {
 
             // toRow -> leftCol - rightCol
-            for(int j=0; j<=rightCol; j++) {
+            for(int j=leftCol; j<=rightCol && totalElements < r*c; j++) {
                 System.out.print(matrix[topRow][j] + " ");
+                totalElements++;
             }
             topRow++;
-
+            
             // rightCol -> topRow - bottomRow
-            for(int i=0; i<=bottomRow; i++){
+            for(int i=topRow; i<=bottomRow && totalElements < r*c; i++){
                 System.out.print(matrix[i][rightCol] + " ");
+                totalElements++;
             }
             rightCol--;
-
+            
             // bottomRow -> rightCol - leftCol
-            for(int j=rightCol; j>=leftCol; j--){
+            for(int j=rightCol; j>=leftCol && totalElements < r*c; j--){
                 System.out.print(matrix[bottomRow][j] + " ");
+                totalElements++;
             }
             bottomRow--;
-
+            
             // leftCol -> bottomRow - topRow
-            for(int i=bottomRow; i>=topRow; i--) {
+            for(int i=bottomRow; i>=topRow && totalElements < r*c; i--) {
                 System.out.print(matrix[i][leftCol] + " ");
+                totalElements++;
             }
             leftCol++;
         }
     }
 
-    public static void main(String agrs[]) {
+    public static void main(String args[]) {
 
+        // user matrix input
         Scanner sc = new Scanner(System.in);
-        System.out.print("Enter Rows and cols: ");
+        System.out.print("Enter No. of Rows and cols: ");
         int r = sc.nextInt();
         int c = sc.nextInt();
+        int[][] matrix = new int[r][c];
+        int total = r*c;
+        System.out.println("Enter " +total + " values ");
+        for(int i =0; i<r; i++) {
+            for(int j=0; j<c; j++) {
+                matrix[i][j] = sc.nextInt();
+            }
+        }
+
+        System.out.println("Input Matrix");
+        printMatrix(matrix);
+
+        System.out.println("Spiral Order");
+        printSpiralOrder(matrix, r, c);
 
         
     }
